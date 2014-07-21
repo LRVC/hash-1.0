c9eb62071213c5555ec1209f0769aee0ce613697
Jeff Taggart <jeff@jetaggart.com>
2014-07-21 11:40:58 -0600

    Print first genre

--- 02_first_genre.rb
+++ 02_first_genre.rb
@@ -1,3 +1,9 @@
 require_relative 'people'
 
 # What is the first genre that each person listed?
+def first_genre(person)
+  person[:preferences][:favorite_genres].first
+end
+
+puts first_genre(PEOPLE["joe"])
+puts first_genre(PEOPLE["sue"])
